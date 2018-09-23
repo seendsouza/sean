2c387563aca3af5e708327cd67bb7351fd74a7a6
Dhrumil <dhrumilp15@gmail.com>
2018-09-23 06:23:46 -0400

    firebase is a good boi now

--- src/components/App.js
+++ src/components/App.js
@@ -5,17 +5,43 @@ import '../stylesheets/App.css';
 import NavBar from './navbar';
 import Footer from './footer';
 import Routes from '../routes';
+import app from "./firebase";
 
 class App extends Component {
+  componentDidMount() {
+    var db = app.database();
+
+    function writeUserData(username, name, email, abstract) {
+      //var new_user_ref = app.database().ref('Users/').push();
+      db.ref('Users/' + username).set({
+        username,
+        abstract,
+        email,
+        name,
+      });
+    }
+    writeUserData("bob", "sam","bob@bob", "bob");
+  }
+
+  /*const db = app.database();
+  function writeUserData(username, name, email, abstract) {
+    //var new_user_ref = app.database().ref('Users/').push();
+    db.ref('Users/' + username).set({
+      username,
+      abstract,
+      email,
+      name,
+    });
+  }
+  writeUserData("bob","bob","bob@bob","bob");*/
   render() {
     return (
       <div className="App">
-      <NavBar />
+      <NavBar/>
         <Routes />
         <Footer/>
       </div>
     );
   }
 }
-
 export default App;
--- src/components/firebase.js
+++ src/components/firebase.js
@@ -1,14 +1,11 @@
-import * as admin from "firebase-admin";
+import firebase from "firebase";
     // Initialize Firebase
-
 var config = {
   apiKey: "AIzaSyD1owOUZqfN4OJd0WRVMBAaiE4bfrNvQJs",
   authDomain: "yorkuhacks-f0a14.firebaseapp.com",
   databaseURL: "https://yorkuhacks-f0a14.firebaseio.com",
-  projectId: "yorkuhacks-f0a14",
   storageBucket: "yorkuhacks-f0a14.appspot.com",
-  messagingSenderId: "465255915789"
 };
-const app = admin.initializeApp(config);
+var app = firebase.initializeApp(config);
 
-export default firebase;
+export default app;
