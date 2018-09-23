7f2fb390773af55823699886e7dd87345ba766a6
Dhrumil <dhrumilp15@gmail.com>
2018-09-23 10:58:23 -0400

    disjointboi

--- src/components/App.js
+++ src/components/App.js
@@ -6,20 +6,18 @@ import Footer from './footer';
 import Routes from '../routes';
 import app from "./firebase";
 
+import indico from 'indico.io';
+
 class App extends Component {
   componentDidMount() {
     var db = app.database();
 
-    function writeUserData(username, name, email, abstract) {
-      //var new_user_ref = app.database().ref('Users/').push();
-      db.ref('Users/' + username).set({
-        username,
-        abstract,
-        email,
-        name,
-      });
-    }
-    writeUserData("busername", "bame","bemail", "babstract");
+    indico.apiKey =  '72e96d569596102c2948c3ace6ad16fe';
+
+    var response = function(res) { console.log(res); }
+    var logError = function(err) { console.log(err); }
+
+    var professions = [anthropology, architecture, art, astronomy, aviation, baseball, bicycling, biology, books, business, cars, climbing, comedy, cooking, crafts, cricket, design, diy, economic_discussion, education, electronics, energy, environmental, fashion, fiction, film, fishing, fitness, gaming, gardening, gender_issues, general_food, golf, health, history, individualist_politics, investment, jobs, math, medicine, military, music, news, nutrition, parenting, personal, personal_care_and_beauty, personalfinance, pets, philosophy, photography, poetry, programming, psychology, realestate, relationships, romance, running, sailing, school, science, scuba, singing, skateboarding, soccer, sports, startups_and_entrepreneurship, surfing, swimming, technology, television, tennis, travel, ultimate, weather, weight_training, wine, wrestling, writing, yoga]
 
     function writeMentorData(mentorname, name, email, tags) {
       //var new_user_ref = app.database().ref('Users/').push();
@@ -40,8 +38,6 @@ class App extends Component {
       //  tagname.push(mentorname : {name : name, email : email})
     }
 
-    writeMentorData("bentorname", "bame","bam@bob", "tag1");
-
     function readMentorsfromTags(tags)
     {
       var dbref = db.ref(`Tags/${tags}/Mentors`);
@@ -49,7 +45,45 @@ class App extends Component {
     }
     readMentorsfromTags("tag1");
 
-  }
+    function writeUserData(username, name, email, abstract) {
+
+      db.ref('Users/' + username).set({
+        username,
+        email,
+        name,
+      });
+
+      indico.textTags(abstract)
+        .then(response)
+        .catch(logError);
+
+      for (var key in response)
+      {
+        if (response[key] >= 0.85)
+        {
+          if (key in professsions)
+          {
+            var tagname2 = db.ref(`Tags/${key}/Users`);
+            tagname2.update({
+              username :{
+                name,
+                email
+              }
+            });
+          }
+        }
+      }
+    }
+    /* batch example
+    var batchInput = [
+        "I love writing code!",
+        "Alexander and the Terrible, Horrible, No Good, Very Bad Day"
+    ];
+    indico.sentiment(batchInput)
+      .then(response)
+      .catch(logError);
+      */
+  }//Ending of ComponentDidMount - VERY VERY IMPORTANT
 
   render() {
     return (
