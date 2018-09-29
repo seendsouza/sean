89c4c629f3eeb2d97e310597c1fcd70963697d6a
Dhrumil Patel <dhrumilp15@gmail.com>
2018-09-29 12:46:06 -0400

    User Signup works

--- src/components/register.js
+++ src/components/register.js
@@ -1,24 +1,73 @@
-import React, {Component} from "react";
-import '../stylesheets/register.css';
+import React, {Component} from "react"
+import '../stylesheets/register.css'
 import {Button} from 'semantic-ui-react'
+import app from './firebase'
 
 class Register extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      Name : '',
+      Pass : '',
+      usertype : 'Mentor',
+      email : '',
+      LvlofEdu : 'Highschool'
+    };
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleInputChange(event) {
+    const target = event.target;
+    const value = event.target.value;
+    const name = event.target.name;
+
+    this.setState({
+      [name] : value
+    })
+  }
+
+  handleSubmit(event) {
+    console.log('Info submitted\nName: ' + this.state.Name + '\nPass: ' + this.state.Pass + '\nUser Type: '
+     + this.state.usertype + '\nEmail: ' + this.state.email + '\nLvlofEdu: ' + this.state.LvlofEdu);
+     event.preventDefault();
+
+    var db = app.database();
+    db.ref(this.state.usertype + '/' + this.state.Name).set({
+      Pass: this.state.Pass,
+      Email : this.state.email,
+      LvlofEdu : this.state.LvlofEdu
+    })
+
+  }
+
   render() {
     return (<div className="Component">
-      <form class="ui form">
+      <form class="ui form" onSubmit = {this.handleSubmit}>
 
         <div class="two fields">
           <div class="field">
             <label>Name:</label>
             <div class='ui input'>
-              <input type='text' placeholder='Name'/>
+              <input
+              name = 'Name'
+              type = 'text'
+              value = {this.state.Name}
+              onChange = {this.handleInputChange}
+              placeholder = "Name"/>
             </div>
           </div>
 
           <div class="field">
             <label>Password:</label>
             <div class='ui input'>
-              <input type='text' placeholder='Name'/>
+              <input
+              name = 'Pass'
+              type='text'
+              value = {this.state.Pass}
+              onChange = {this.handleInputChange}
+              placeholder='Password'/>
             </div>
           </div>
         </div>
@@ -26,7 +75,10 @@ class Register extends Component {
         <div class="field">
           <label>Mentor/User:</label>
           <div class='ui buttons'>
-            <select class="" name="">
+            <select class=""
+            name='usertype'
+            value = {this.state.usertype}
+            onChange = {this.handleInputChange}>
               <option value="Mentor">Mentor</option>
               <option value="User">User</option>
             </select>
@@ -36,22 +88,29 @@ class Register extends Component {
         <div class="field">
           <label>Email:</label>
           <div class='ui input'>
-            <input type='text' placeholder='Email'/>
+            <input
+            name = 'email'
+            type='text'
+            value = {this.state.email}
+            onChange = {this.handleInputChange}
+            placeholder='Email'/>
           </div>
         </div>
 
         <div class="field">
           <label>Level of Education:</label>
           <div class='ui input'>
-          <select class="" name="">
+          <select class=""
+          name='LvlofEdu'
+          value = {this.state.LvlofEdu}
+          onChange = {this.handleInputChange}>
             <option value="Highschool">Highschool</option>
             <option value="University Undergrad">Universty Undergrad</option>
             <option value="Professor/Doctor">Professor/Doctor</option>
           </select>
         </div>
         </div>
-        <Button type="submit" class="button">Submit</Button>
-
+        <Button type="submit" class="button" value = "Submit">Submit</Button>
     </form>
   </div>);
   }
