98c2eaa8a5c09559db02195450d059c739a1f1c8
Dhrumil Patel <dhrumilp15@gmail.com>
2018-10-06 16:03:51 -0400

    pushing

--- src/App.js
+++ src/App.js
@@ -1,17 +1,9 @@
 import React, { Component } from 'react';
-<<<<<<< HEAD
 // move logo to public folder
 import './stylesheets/App.css';
 import NavBar from './components/navbar';
 import Footer from './components/footer';
 import Routes from './routes';
-=======
-import logo from './logo.svg';
-import './App.css';
-import NavBar from './navbar';
-import Footer from './footer';
-import './firebase';
->>>>>>> f8e6efa7da1e82d7f9c9cc07a7de2fc3400cb57f
 
 class App extends Component {
   render() {
